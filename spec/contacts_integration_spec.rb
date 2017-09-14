16ce1e4d0aca97c9bc2ed8e9f276be9efad7e585
Dan Danilyuk and Nate McGregor <dan@>
2017-09-14 11:39:00 -0700

    get tests passing

---
+++ spec/contacts_integration_spec.rb
@@ -0,0 +1,28 @@
+
+require('capybara/rspec')
+require('./app')
+Capybara.app = Sinatra::Application
+set(:show_excepeptions, false)
+
+describe('list path', {:type => :feature}) do
+  it "will display user input into list" do
+    visit('/')
+    fill_in('name', :with => 'Nate')
+    fill_in('last', :with => 'McGregor')
+    fill_in('phone', :with => '503-747-3455')
+    fill_in('street', :with => '2424 Main St')
+    fill_in('city', :with => 'Portland')
+    fill_in('state', :with => 'OR')
+    fill_in('zip', :with => '97485')
+
+    click_button('Go!')
+    expect(page).to have_content('McGregor, Nate')
+  end
+  it "display name of clicked item" do
+    visit('/')
+    click_link('McGregor, Nate')
+    expect(page).to have_content('Name: Nate McGregor')
+    expect(page).to have_content('phone: 503-747-3455')
+    expect(page).to have_content('address: 2424 Main St ,Portland, OR, 97485')
+  end
+end
--- spec/contacts_spec.rb
+++ spec/contacts_spec.rb
@@ -17,7 +17,7 @@ describe("Contact") do
 
   describe("#save") do
     it("saves an contact to the list of items") do
-      contact = Contact.new("dan","111.111.1111","123 road","Portland","OR","97206")
+      contact = Contact.new("dan","danilyuk","111.111.1111","123 road","Portland","OR","97206")
       contact.save()
       expect(Contact.all()).to(eq([contact]))
     end
@@ -25,7 +25,7 @@ describe("Contact") do
 
   describe(".clear") do
     it("clears all contact from the list") do
-      contact = Contact.new("dan","111.111.1111","123 road","Portland","OR","97206")
+      contact = Contact.new("dan","danilyuk","111.111.1111","123 road","Portland","OR","97206")
       contact.save()
       Contact.clear()
       expect(Contact.all()).to(eq([]))
@@ -34,9 +34,9 @@ describe("Contact") do
 
   describe("#id") do
     it("increments an id by 1 each time a new contact is added") do
-      contact = Contact.new("dan","111.111.1111","123 road","Portland","OR","97206")
+      contact = Contact.new("dan","danilyuk","111.111.1111","123 road","Portland","OR","97206")
       contact.save()
-      contact2 = Contact.new("bob","222.222.2222","456 street","Portland","OR","97207")
+      contact2 = Contact.new("bob","lewis","222.222.2222","456 street","Portland","OR","97207")
       contact2.save()
       expect(contact.id()).to(eq(1))
       expect(contact2.id()).to(eq(2))
@@ -44,9 +44,9 @@ describe("Contact") do
   end
   describe(".find") do
     it("finds an contact based on its id") do
-      contact = Contact.new("dan","111.111.1111","123 road","Portland","OR","97206")
+      contact = Contact.new("dan","danilyuk","111.111.1111","123 road","Portland","OR","97206")
       contact.save()
-      contact2 = Contact.new("bob","222.222.2222","456 street","Portland","OR","97207")
+      contact2 = Contact.new("bob","lewis","222.222.2222","456 street","Portland","OR","97207")
       contact2.save()
       expect(Contact.find(1)).to(eq(contact))
       expect(Contact.find(2)).to(eq(contact2))
